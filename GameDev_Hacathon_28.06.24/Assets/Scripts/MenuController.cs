1757dca1eadf499a4bf37d45b889cddeb7487b87
DukeTime <duke.of.times@gmail.com>
2024-06-28 19:44:38 +0300

    scenes

--- GameDev_Hacathon_28.06.24/Assets/Scripts/MenuController.cs
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/MenuController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject button1;
+    [SerializeField] private GameObject button2;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PlayButton()
+    {
+        background.transform.position = new Vector3(-480, 0, 0);
+        button1.transform.position = new Vector3(-480, 0, 0);
+        button2.transform.position = new Vector3(-480, 0, 0);
     }
 }
