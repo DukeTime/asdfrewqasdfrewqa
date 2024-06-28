e3bf3f748f5a5f65ddd49fc36395a47d48789c8f
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-06-28 21:26:05 +0300

    shop started

---
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/BackToMenuButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenuButtonScript : MonoBehaviour
+{
+
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    
+
+
+    public int money;
+    
+    [SerializeField] public GameObject myObject;
+
+    Text myText;
+
+    void Start()
+    {
+        myText = myObject.GetComponent<Text>();
+
+        money = PlayerPrefs.GetInt("money");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = money.ToString();
+    }
+}
