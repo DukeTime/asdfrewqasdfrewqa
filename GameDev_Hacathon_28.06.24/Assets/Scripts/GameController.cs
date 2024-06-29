cb275b2d64d6871c79458dfe3db7f145d3695f3c
DukeTime <duke.of.times@gmail.com>
2024-06-30 00:34:00 +0300

    damage

--- GameDev_Hacathon_28.06.24/Assets/Scripts/GameController.cs
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/GameController.cs
@@ -3,26 +3,31 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
     using UnityEngine.SceneManagement;
 
     public class GameController : MonoBehaviour
     {
         //[SerializeField] private WaterPropertyBlockSetter water;
         public int game_phase = 0;
+        public float max_rod_hp = 100;
+        public float rod_hp;
         [SerializeField] private Spawner spawner_script;
         [SerializeField] private Camera game_camera;
         [SerializeField] private GameObject boat;
         [SerializeField] private Animator boat_animator;
         [SerializeField] private Animator succes_animation;
+        [SerializeField] private Image hp_bar;
         System.Random ran = new System.Random();
         private Moving moving_script;
         private GameObject fish;
         void Start()
         {
+            rod_hp = max_rod_hp;
             //water = GameObject.FindGameObjectWithTag("Water").GetComponent<WaterPropertyBlockSetter>();
         }
 
-        void Update()
+        void FixedUpdate()
         {
             if (game_phase == 0)
             {
@@ -39,7 +44,20 @@
                     fish.SetActive(false);
                     EndFishing(moving_script.FishType);
                 }
+                if (transform.position.x - moving_script.start_fish_x > 11)
+                {
+                    FisherDamage();
+                }
+                else if (transform.position.x - moving_script.start_fish_x < -11)
+                {
+                    FisherDamage();
+                }
             }
+        }
+        private void FisherDamage()
+        {
+            rod_hp -= 0.5f;
+            hp_bar.transform.localScale = hp_bar.transform.localScale + new Vector3(-1, 0, 0);
         }
 
         private void ShipStop()
