5496640cce7bc747bab8c6be7ee3597c1ecedeb8
asdfrewqha <asdfrewqha@gmail.com>
2024-06-29 20:31:06 +0300

    aaa

--- GameDev_Hacathon_28.06.24/Assets/Scripts/GameController.cs
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/GameController.cs
@@ -17,7 +17,6 @@
         System.Random ran = new System.Random();
         private Moving moving_script;
         private GameObject fish;
-        private GameObject splash;
         void Start()
         {
             //water = GameObject.FindGameObjectWithTag("Water").GetComponent<WaterPropertyBlockSetter>();
@@ -46,8 +45,6 @@
         {
             boat_animator.SetBool("going", false);
             fish = GameObject.FindGameObjectWithTag("Fish");
-            splash = GameObject.FindGameObjectWithTag("Splash");    
-            moving_script = splash.GetComponent<Moving>();
             moving_script = fish.GetComponent<Moving>();
 
             moving_script.StopMoving = true;
