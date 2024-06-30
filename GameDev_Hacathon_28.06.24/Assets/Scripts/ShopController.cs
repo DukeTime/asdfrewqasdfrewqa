3b06166e8ed3bab04c2a6110721c716b6209cbcb
asdfrewqha <asdfrewqha@gmail.com>
2024-06-30 14:31:37 +0300

    fixshop

--- GameDev_Hacathon_28.06.24/Assets/Scripts/ShopController.cs
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/ShopController.cs
@@ -16,7 +16,7 @@ public class ShopController : MonoBehaviour
     [SerializeField] public GameObject button;
 
     Text myText;
-
+    public string type;
     void Start()
     {
         myText = myObject.GetComponent<Text>();
@@ -37,11 +37,13 @@ public class ShopController : MonoBehaviour
             myText.text = mon.ToString() + "k";
         }
     }
-    public void ButtonClick(int lvl, string type)
+    public void ButtonClckType(string typ){
+        type = typ;
+    }
+    public void ButtonClick(int lvl)
     {
         money = PlayerPrefs.GetInt("money");
         if (money >= 100){
-            
             PlayerPrefs.SetInt(type, lvl);
             money = money - 100;
             PlayerPrefs.SetInt("money",money);
