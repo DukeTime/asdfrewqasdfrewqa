36e2c3679deac20fa709e96a92380fc33eb0391c
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-07-05 22:51:10 +0300

    fg

--- GameDev_Hacathon_28.06.24/Assets/Scripts/BuyController.cs
+++ GameDev_Hacathon_28.06.24/Assets/Scripts/BuyController.cs
@@ -5,47 +5,73 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class BuyController : MonoBehaviour
 {
-    int is_bought;
+
 
     	
-    [SerializeField] private Dictionary<string, int> prices = new Dictionary<string, int>()
-    {
-        {"boat1", 100},
-        {"boat2", 300},
-        {"boat3", 500},
-        {"boat4", 700},
-        {"rod1", 100},
-        {"rod2", 300},
-        {"rod3", 500},
-        {"rod4", 700}
 
-    };
 
     [SerializeField] public GameObject Button;
 
-    [SerializeField] public GameObject UpgradeText;
+    [SerializeField] public Image Image;
+
+    [SerializeField] public Text UpgradeText;
 
     [SerializeField] public GameObject Price;
 
-    // string name_;
+    [SerializeField] public Sprite lvl1;
 
-    // Debug.Log(Button.name);
+    [SerializeField] public Sprite lvl2;
+
+    [SerializeField] public Sprite lvl3;
+
+    [SerializeField] public Sprite lvl4;
 
 
-    // if (Button.name == "BoatUpgradeButton"){
-    //     name = "boatlvl";
-    // }
-
-
-    // if (Button.name == "RodUpgradeButton") {
-    //     name = "rodlvl";
-    // }
-
-    // int currentlvl = PlayerPrefs.GetInt(name+"lvl") + 1;
+    int currentlvl;
+    string name;
 
     void Start()
     {
-        
+
+
+
+    if (Button.name == "BoatUpgradeButton"){
+        name = "boatlvl";
+    }
+
+
+    if (Button.name == "RodUpgradeButton") {
+        name = "rodlvl";
+    }
+
+    Debug.Log(name);
+    currentlvl = PlayerPrefs.GetInt(name+"lvl") + 1;
+    Debug.Log(currentlvl);
+
+
+    if (currentlvl == 1){
+       Image.sprite = lvl1;
+        UpgradeText.text = "Upgrade boat to level 2";
+    }
+
+    if (currentlvl == 2){
+       Image.sprite = lvl2;
+        UpgradeText.text = "Upgrade boat to level 3";
+    }
+
+    if (currentlvl == 2){
+       Image.sprite = lvl2;
+        UpgradeText.text = "Upgrade boat to level 4";
+    }
+
+    if (currentlvl == 2){
+       Image.sprite = lvl2;
+        UpgradeText.text = "Completely upgraded";
+    }
+
+
+
+
     }
 
     public void BuyItem()
@@ -53,11 +79,16 @@ public class BuyController : MonoBehaviour
         int balance = PlayerPrefs.GetInt("money");
 
 
-        Debug.Log("bought" + this.gameObject.name);
         
-        
-        
+        if (currentlvl < 4 & balance >= (currentlvl*200 + 100)){
 
+
+            PlayerPrefs.SetInt(name+"lvl",currentlvl);
+
+        }
+        
+        
+        Debug.Log(currentlvl);
         SceneManager.LoadScene(3);
 
 
